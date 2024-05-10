acbfae6e1e3c4de2cd3db7a12c84966ccce25e2a
Thimote147 <lelemmingadorable@gmail.com>
2024-05-10 22:28:36 +0200

    Adding search filter for calendars

--- routes/calendars.js
+++ routes/calendars.js
@@ -4,12 +4,15 @@ const { generateCalendar } = require("./functions/function.js");
 const Gestion = require("../models/Gestion.js");
 const Calendar = require("../models/Calendar.js");
 
+let month = ["Décembre", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre"];
+
 router.get("/", (req, res) => {
     if (req.session.connected) {
         const date = new Date().toJSON();
         const workingCalendar = generateCalendar(date.slice(0, 4), date.slice(5, 7), 'working');
         const notWorkingCalendar = generateCalendar(date.slice(0, 4), date.slice(5, 7), 'not working');
-        res.render("calendars.hbs", { working: workingCalendar, notWorking: notWorkingCalendar, members: Gestion.allMembers() });
+
+        res.render("calendars.hbs", { month: month[date.slice(5, 7)%12] , working: workingCalendar, notWorking: notWorkingCalendar, members: Gestion.allMembers() });
     } else {
         res.render("login.hbs");
     }
@@ -28,6 +31,18 @@ router.post("/add_unavailability", (req, res) => {
     }
 
     res.redirect("/calendars#unavailability");
+});
+
+router.get('/filter', (req, res) => {
+    if (req.session.connected) {
+        const date = new Date(req.query.month).toJSON();
+        const workingCalendar = generateCalendar(date.slice(0, 4), date.slice(5, 7), 'working');
+        const notWorkingCalendar = generateCalendar(date.slice(0, 4), date.slice(5, 7), 'not working');
+        
+        res.render("calendars.hbs", { month: month[date.slice(5, 7) % 12], working: workingCalendar, notWorking: notWorkingCalendar, members: Gestion.allMembers() });
+    } else {
+        res.render('login.hbs');
+    }
 });
 
 module.exports = router;
