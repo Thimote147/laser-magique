fb87ab9b2a2d8a6a474c78d499b4ffccca235b66
Fétu Thimoté <lelemmingadorable@gmail.com>
2024-06-11 22:01:25 +0200

    Fix stock counter

--- models/Stock.js
+++ models/Stock.js
@@ -5,7 +5,7 @@ module.exports.list = () => db.prepare("SELECT * FROM stocks ORDER BY name").all
 module.exports.add = (name, quantity) => {
     const stmt = db.prepare("SELECT quantity FROM stocks WHERE name = ?").get(name);
     
-    const result = stmt.quantity + quantity;
+    const result = parseInt(stmt.quantity) + parseInt(quantity);
     
     db.prepare("UPDATE stocks SET quantity = ? WHERE name = ?").run(result, name);
 };
