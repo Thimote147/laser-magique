c699dfe79b49daa6fd621f73a4eeafa9cded21ad
Fétu Thimoté <thimote.fetu@student.vinci.be>
2024-10-31 12:16:55 +0100

    fix: responsive

--- Frontend/src/components/Activities/Details.tsx
+++ Frontend/src/components/Activities/Details.tsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
 import { DetailsProps } from "./types";
 
 const Details = ({ name, description, details, image }: DetailsProps) => {
     return (
-        <div style={{ backgroundImage: `url(${image})` }}>
+        <div className="w-[400px] sm:w-[450px] md:w-[30%] h-[220px] xl:h-wo-menu text-center flex flex-col justify-end items-center" style={{ 
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderRadius: "20px",
+         }}>
             <h1>{name}</h1>
             <p>{description}</p>
+            <hr className="border-[#64b357]"/>
             <p>{details}</p>
+            <Link to="/" className="border w-[150px]">En savoir plus</Link>
         </div>
     )
 }
--- Frontend/src/components/Home/index.tsx
+++ Frontend/src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import Menu from "../Menu";
 
 const Home = () => {
     return (
-        <main className="relative w-full h-screen flex items-start md:items-center justify-center px-4 py-10">
+        <main className="relative w-full h-screen flex items-start justify-center px-4 py-10">
             <Activities />
             <Menu />
         </main>
