584ee2dcb5aba4a4bfd6035d298816a569a2cbdd
Thimote147 <lelemmingadorable@gmail.com>
2024-11-12 18:22:42 +0100

    Modify: ResList css

--- Frontend/src/components/DayRes/ResList.tsx
+++ Frontend/src/components/DayRes/ResList.tsx
@@ -4,11 +4,12 @@ import { format, parseISO } from "date-fns";
 
 interface ResListProps {
     view: "calendar" | "flash";
+    clickHours?: string | null;
     reservations: { reservation_id: number; firstname: string; lastname: string | null; nbr_pers: number; group_type: string; date: string; }[];
     setReservations: React.Dispatch<React.SetStateAction<{ reservation_id: number; firstname: string; lastname: string | null; nbr_pers: number; group_type: string; date: string; }[]>>;
 }
 
-const ResList = ({ view, reservations, setReservations }: ResListProps) => {
+const ResList = ({ view, clickHours, reservations, setReservations }: ResListProps) => {
     const handleDeleteReservation = (id: number) => {
         // Implement delete functionality with API call
         setReservations((prev) => prev.filter((res) => res.reservation_id !== id));
@@ -21,34 +22,94 @@ const ResList = ({ view, reservations, setReservations }: ResListProps) => {
 
     return (
         <AnimatePresence>
-            {view === "flash" && (
-                reservations.map((res, index) => (
-                    <motion.div
-                        key={res.reservation_id}
-                        initial={{ opacity: 0, y: 10 }}
-                        animate={{
-                            opacity: 1,
-                            y: 0,
-                            transition: { delay: index * 0.05 },
-                        }}
-                        exit={{ opacity: 0, y: 10 }}
-                        transition={{ duration: 0.2 }}
-                        className="w-full flex items-center justify-between border border-[#454545] rounded-lg p-2"
-                    >
-                        <div className="w-full flex flex-col gap-3 ml-1">
-                            <p className="text-white font-semibold text-xl">{res.firstname}</p>
-                            <span className="text-xs">{format(parseISO(res.date), "HH:mm")}</span>
-                            <span className="text-xs">{res.group_type} de {res.nbr_pers} personnes</span>
-                        </div>
-                        <div className="w-full flex flex-col items-end">
-                            <ResButton res_id={res.reservation_id} name="Activité" style="text-white/50 border-white" />
-                            <section className="flex">
-                                <ResButton res_id={res.reservation_id} name="Modifier" style="text-white hover:bg-zinc-600 border-white" handle={handleEditReservation} />
-                                <ResButton res_id={res.reservation_id} name="Supprimer" style="text-red-500 hover:bg-red-700 border-red-500" handle={handleDeleteReservation} />
-                            </section>
-                        </div>
-                    </motion.div>
-                )))}
+            {view === "flash" ? (
+                <>
+                    <div className="w-full flex items-center justify-end mb-2">
+                        <span className="text-white/40 text-sm">
+                            {reservations.length} réservation
+                            {reservations.length > 1 && "s"}
+                        </span>
+                    </div>
+                    {
+                        reservations.map((res, index) => (
+                            <motion.div
+                                key={res.reservation_id}
+                                initial={{ opacity: 0, y: 10 }}
+                                animate={{
+                                    opacity: 1,
+                                    y: 0,
+                                    transition: { delay: index * 0.05 },
+                                }}
+                                exit={{ opacity: 0, y: 10 }}
+                                transition={{ duration: 0.2 }}
+                                className="w-full flex items-center justify-between border border-[#454545] rounded-lg p-2"
+                            >
+                                <div className="w-full flex flex-col gap-3 ml-1">
+                                    <p className="text-white font-semibold text-xl">{res.firstname}</p>
+                                    <span className="text-xs">{format(parseISO(res.date), "HH:mm")}</span>
+                                    <span className="text-xs">{res.group_type} de {res.nbr_pers} personnes</span>
+                                </div>
+                                <div className="w-full flex flex-col items-end">
+                                    <ResButton res_id={res.reservation_id} name="Activité" style="text-white/50 border-white" />
+                                    <section className="flex">
+                                        <ResButton res_id={res.reservation_id} name="Modifier" style="text-white hover:bg-zinc-600 border-white" handle={handleEditReservation} />
+                                        <ResButton res_id={res.reservation_id} name="Supprimer" style="text-red-500 hover:bg-red-700 border-red-500" handle={handleDeleteReservation} />
+                                    </section>
+                                </div>
+                            </motion.div>
+                        ))}
+                </>
+            ) : (
+                <AnimatePresence>
+                    {clickHours && (
+                        <>
+                            <div className="w-full flex items-center justify-between my-2">
+                                <div className="flex items-center">
+                                    <div
+                                        className="flex items-center justify-center p-2 rounded bg-zinc-700/30"
+                                    >
+                                        <span className="font-medium text-sm text-white">
+                                            {clickHours}
+                                        </span>
+                                    </div>
+                                </div>
+                                <span className="text-white/40 text-sm">
+                                    {reservations.length} réservation
+                                    {reservations.length > 1 && "s"}
+                                </span>
+                            </div>
+                            {reservations.map((res, index) => (
+                                <motion.div
+                                    key={res.reservation_id}
+                                    initial={{ opacity: 0, y: 10 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: { delay: index * 0.05 },
+                                    }}
+                                    exit={{ opacity: 0, y: 10 }}
+                                    transition={{ duration: 0.2 }}
+                                    className="w-full flex items-center justify-between border border-[#454545] rounded-lg p-2"
+                                >
+                                    <div className="min-w-36">
+                                        <p className="text-white font-semibold text-xl">
+                                            {res.firstname}
+                                        </p>
+                                        <span className="text-xs">{res.group_type} <br /> de {res.nbr_pers} personnes</span>
+                                    </div>
+                                    <div className="w-full flex flex-col items-end">
+                                        <ResButton res_id={res.reservation_id} name="Activité" style="text-white/50 border-white" />
+                                        <section className="flex">
+                                            <ResButton res_id={res.reservation_id} name="Modifier" style="text-white hover:bg-zinc-600 border-white" handle={handleEditReservation} />
+                                            <ResButton res_id={res.reservation_id} name="Supprimer" style="text-red-500 hover:bg-red-700 border-red-500" handle={handleDeleteReservation} />
+                                        </section>
+                                    </div>
+                                </motion.div>
+                            ))}
+                        </>
+                    )}
+                </AnimatePresence>
+            )}
         </AnimatePresence>
     )
 };
--- Frontend/src/components/DayRes/index.tsx
+++ Frontend/src/components/DayRes/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { format, parseISO } from "date-fns";
 import Header from "./Header";
-import ResButton from "./ResButton";
 import ResList from "./ResList";
 
 const DayRes = () => {
@@ -33,16 +32,6 @@ const DayRes = () => {
             setClickHours(hour);
             setFilteredReservations(filterReservations(hour));
         }
-    };
-
-    const handleDeleteReservation = (id: number) => {
-        // Implement delete functionality with API call
-        setReservations((prev) => prev.filter((res) => res.reservation_id !== id));
-    };
-
-    const handleEditReservation = (id: number) => {
-        // Implement edit functionality
-        console.log("Edit reservation with ID:", id);
     };
 
     const gridCols = () => {
@@ -142,7 +131,7 @@ const DayRes = () => {
                                 {Object.keys(hours).map((hour) => (
                                     <motion.div layout className="w-full relative" key={hour}>
                                         <motion.button
-                                            className="flex items-center justify-center w-full h-[59px] rounded-lg bg-zinc-700/30 cursor-pointer duration-300 transition-opacity"
+                                            className="flex items-center justify-center w-full h-[59px] rounded-lg bg-zinc-700/30 cursor-pointer duration-300 transition-opacity mb-2"
                                             onClick={() => handleMouseClick(hour)}
                                         >
                                             <span className="font-semibold text-lg text-white">
@@ -152,61 +141,7 @@ const DayRes = () => {
                                     </motion.div>
                                 ))}
                             </motion.div>
-                            <AnimatePresence>
-                                {clickHours && (
-                                    <div className="top-full mt-2 w-[410px] flex flex-col items-center justify-center gap-2 border border-[#1e1e1e] rounded-xl p-4 bg-[#242424]">
-                                        <div className="w-full flex items-center justify-between mb-2">
-                                            <div className="flex items-center gap-2">
-                                                <div
-                                                    className="flex items-center justify-center size-7 rounded bg-zinc-700/30"
-                                                >
-                                                    <span className="font-medium text-sm text-white">
-                                                        {currentDate.split(" ")[1]}
-                                                    </span>
-                                                </div>
-                                                <span className="font-medium text-sm text-white">
-                                                    {currentDate.split(" ")[0]}
-                                                </span>
-                                                <span className="font-medium text-xs text-white/70 block mt-0.5">
-                                                    {currentDate.split(" ")[1] + " " + currentDate.split(" ")[2]}
-                                                </span>
-                                            </div>
-                                            <span className="text-white/40 text-sm">
-                                                {filteredReservations.length} réservation
-                                                {filteredReservations.length > 1 && "s"}
-                                            </span>
-                                        </div>
-                                        {filteredReservations.map((res, index) => (
-                                            <motion.div
-                                                key={res.reservation_id}
-                                                initial={{ opacity: 0, y: 10 }}
-                                                animate={{
-                                                    opacity: 1,
-                                                    y: 0,
-                                                    transition: { delay: index * 0.05 },
-                                                }}
-                                                exit={{ opacity: 0, y: 10 }}
-                                                transition={{ duration: 0.2 }}
-                                                className="w-full flex items-center justify-between border border-[#454545] rounded-lg p-2"
-                                            >
-                                                <div className="min-w-36">
-                                                    <p className="text-white font-semibold text-xl">
-                                                        {res.firstname}
-                                                    </p>
-                                                    <span className="text-xs">{res.group_type} <br /> de {res.nbr_pers} personnes</span>
-                                                </div>
-                                                <div className="w-full flex flex-col items-end">
-                                                    <ResButton res_id={res.reservation_id} name="Activité" style="text-white/50 border-white" />
-                                                    <section className="flex">
-                                                        <ResButton res_id={res.reservation_id} name="Modifier" style="text-white hover:bg-zinc-600 border-white" handle={handleEditReservation} />
-                                                        <ResButton res_id={res.reservation_id} name="Supprimer" style="text-red-500 hover:bg-red-700 border-red-500" handle={handleDeleteReservation} />
-                                                    </section>
-                                                </div>
-                                            </motion.div>
-                                        ))}
-                                    </div>
-                                )}
-                            </AnimatePresence>
+                            <ResList view={view} clickHours={clickHours} reservations={filteredReservations} setReservations={setReservations}/>
                         </AnimatePresence>
                     )}
                 </motion.div>
