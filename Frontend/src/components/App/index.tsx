df6e244f7f2c8e75b274a13a675bcdd34ccc254b
Fétu Thimoté <thimote.fetu@student.vinci.be>
2024-11-14 14:12:01 +0100

    Add: reviews front

--- Frontend/src/components/App/index.tsx
+++ Frontend/src/components/App/index.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from '../Home';
 import Gestion from '../Gestion';
 import Reservation from '../Reservation';
+import Reviews from '../Reviews';
+import FindRes from '../Reviews/FindRes';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/gestion" element={<Gestion />} />
         <Route path="/reservation/:id" element={<Reservation />} />
+        <Route path="/reviews" element={<FindRes />} />
+        <Route path="/reviews/:id" element={<Reviews />} />
         <Route path="*" element={<Link to="/" className='border'>Retour accueil</Link>} />
       </Routes>
     </Router>
