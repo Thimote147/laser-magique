a9b9e95103a53e96ea8573ac591ca4920d181179
Fétu Thimoté <165772108+thimote-fetu-vinci@users.noreply.github.com>
2024-11-14 20:47:19 +0100

    Add: Laser Game section

--- Frontend/src/components/Home/index.tsx
+++ Frontend/src/components/Home/index.tsx
@@ -5,7 +5,7 @@ import Welcome from "../Welcome";
 
 const Home = () => {
     return (
-        <main className="relative w-full min-h-screen flex flex-col items-center justify-start md:pt-9 pb-20">
+        <main className="relative w-full min-h-screen flex flex-col items-center justify-start md:pt-9">
             <Menu />
             <Activities />
             <Welcome/>
