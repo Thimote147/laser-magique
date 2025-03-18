0b3394aabc47a1b623cb13a84f035b0317cbefad
Thimote147 <thimotefetu@gmail.com>
2025-03-19 00:25:52 +0100

    Add Consumptions and Stock pages with navbar integration and layout adjustments

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
-      <Box component="main">
+      <Box component="main" sx={{ pt: '64px' }}> {/* Add 64px padding top to account for fixed navbar */}
         <Outlet />
       </Box>
       <Footer />
